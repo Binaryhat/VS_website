c5fd4e5bbe545bf93cc788f0b37970442bc25480
Mahesh Langote <maheshl@versionsolutions.com>
2018-06-22 17:50:45 +0530

    home  page details

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+$(document).ready(function () {
+    //for preloader to all pages
+    $(window).on("load",function () { // makes sure the whole site is loaded
+        var preloader = $('#mask div');
+        preloader.fadeOut('slow'); // will first fade out the loading animation
+        $('#mask').delay(350).fadeOut('slow');
+        // will fade out the white DIV that covers the website.
+        $('body').delay(350).css({
+            'overflow': 'visible'
+        });
+    });
+    
+    
+    // scroll
+    $(window).scroll(function(){
+        var scroll = $(window).scrollTop();
+        if (scroll > 600) {
+          $(".main-navbar").addClass("main-navbar--white");
+          $(".navbar-brand img").attr('src','imgs/logo.png');
+        }
+  
+        else{
+            $(".main-navbar").removeClass("main-navbar--white");  	
+            $(".navbar-brand img").attr('src','imgs/logo_white.png');
+        }
+    })
+
+
+});
